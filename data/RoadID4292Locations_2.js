ce02c2aa5b2933d1e5e9bfe9a3ad4f1fa266aa98
sreejithgh <sreejithgh@yahoo.com>
2019-06-19 00:13:10 -0400

    First Commit

---
+++ data/RoadID4292Locations_2.js
@@ -0,0 +1,19 @@
+var json_RoadID4292Locations_2 = {
+"type": "FeatureCollection",
+"name": "RoadID4292Locations_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.194558899092456, 15.733807400273573 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.271454669445177, 15.864288796480727 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.124881690624591, 15.680988739257836 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.194558899092399, 15.733807400273509 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.27145466940226, 15.864288796526983 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.793828805601763, 15.819406006669841 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 100.793828805601763, 15.819406006669853 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 101.324976322374042, 15.799783378502511 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 101.766717541594005, 15.7428256134443 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 102.003526328359001, 15.7933260463839 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 101.324976322374042, 15.799783378502511 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ 101.766717541593607, 15.742825613444293 ] } }
+]
+}
